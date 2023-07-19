823bb45fa4e2293f5ffa1c66311cd681c26b3286
kvzon1984 <ivega.josue@gmail.com>
2023-07-18 22:51:53 -0400

    fix: se realiza curso de principios SOLID

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import './style.css'
+// import './clean-code/07-tarea';
+// import './code-smells/02-low-coupling';
+import './solid/02-open-close-a'
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+app.innerHTML = `
+  <h1>CleanCode y SOLID</h1>
+  <span>Revisar la consola de JavaScript</span>
+`
+
